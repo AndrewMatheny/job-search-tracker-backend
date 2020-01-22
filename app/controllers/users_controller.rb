c5149b9c6dc2ce13038f73fd1ac3b2ffbbb9ee58
mia-pan <miapanyuewei@gmail.com>
2020-01-21 16:28:22 -0800

    create user and update job method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,16 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         render :json => @user
     end
+
+    def create 
+        @user = User.new(user_params)
+        @user.save 
+        render :json => @user
+    end 
+
+    private 
+    def user_params
+        params.require(:user).permit(:name, :age, :resume, :linkedin)
+    end 
+
 end
