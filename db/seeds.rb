17f27fbd9734724d0ffb546d5fdeb84561d136c9
AndrewMatheny <github email address>
2020-01-23 11:52:42 -0800

    fixed bugs with seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,65 +8,66 @@
 require 'faker'
 require 'date'
 
-drew = User.create(name: "Drew", age: 27, resume: "Test Resume", linkedin: "https://www.linkedin.com/in/drew-matheny-84760b97/")
+drew = User.find_or_create_by(name: "Drew", age: 27, resume: "Test Resume", linkedin: "https://www.linkedin.com/in/drew-matheny-84760b97/")
 date1 = DateTime.new(2020, 1, 20)
 date2 = DateTime.new(2020, 1, 21)
 date3 = DateTime.new(2020, 1, 22)
 date4 = DateTime.new(2020, 1, 23)
 date5 = DateTime.new(2020, 1, 24)
 
-Job.create(title: "Amazon Software Developer", description: "Full Stack Developer", status: "Waiting for phone interview", rating: 5, application_date: date1, source: "indeed.com", user_id: drew.id)
-Job.create(title: "Google Software Developer", description: "Full Stack Developer", status: "In person interview", rating: 5, application_date: date5, source: "indeed.com", user_id: drew.id)
-Job.create(title: "Microsoft Software Developer", description: "Frontend Developer", status: "Waiting to hear back", rating: 5, application_date: date4, source: "monster.com", user_id: drew.id)
-Job.create(title: "Qualtrics Software Developer", description: "Backend Developer", status: "Technical Interview", rating: 5, application_date: date2, source: "Glenn", user_id: drew.id)
-Job.create(title: "Netflix Software Developer", description: "Full Stack Developer", status: "Waiting for phone interview", rating: 5, application_date: date1, source: "indeed.com", user_id: drew.id)
+Job.find_or_create_by(title: "Amazon Software Developer", description: "Full Stack Developer", status: "Waiting for phone interview", rating: 5, application_date: date1, source: "indeed.com", user_id: drew.id)
+Job.find_or_create_by(title: "Google Software Developer", description: "Full Stack Developer", status: "In person interview", rating: 5, application_date: date5, source: "indeed.com", user_id: drew.id)
+Job.find_or_create_by(title: "Microsoft Software Developer", description: "Frontend Developer", status: "Waiting to hear back", rating: 5, application_date: date4, source: "monster.com", user_id: drew.id)
+Job.find_or_create_by(title: "Qualtrics Software Developer", description: "Backend Developer", status: "Technical Interview", rating: 5, application_date: date2, source: "Glenn", user_id: drew.id)
+Job.find_or_create_by(title: "Netflix Software Developer", description: "Full Stack Developer", status: "Waiting for phone interview", rating: 5, application_date: date1, source: "indeed.com", user_id: drew.id)
 
 
 
-10.times.do 
-   User.create(
-       name: Faker::Name.name, 
-       age: Faker::PhoneNumber.subscriber_number(length: 2),
-       resume: Faker::LoremPixel.image(size: "50x60"),
-       linkedin: Faker::LoremPixel.image(size: "50x60", is_gray: true)
-   )
-end 
+# 10.times.do 
+#    User.create(
+#        name: Faker::Name.name, 
+#        age: Faker::PhoneNumber.subscriber_number(length: 2),
+#        resume: Faker::LoremPixel.image(size: "50x60"),
+#        linkedin: Faker::LoremPixel.image(size: "50x60", is_gray: true)
+#    )
+# end 
 
-10.times.do
-    job.create(title: Faker::Job.title,
-    description: Faker::Job.field
-    status: "Waiting for phone interview",
-    rating: 5,
-    application_date: date1,
-    source: "indeed.com",
-    user_id: Faker::Name.name.id
-    )
-end 
+# 10.times.do
+#     job.create(title: Faker::Job.title,
+#     description: Faker::Job.field
+#     status: "Waiting for phone interview",
+#     rating: 5,
+#     application_date: date1,
+#     source: "indeed.com",
+#     user_id: Faker::Name.name.id
+#     )
+# end 
 
-mia = User.create(name: "Mia", age: 24, resume: "Resume on google docs", linkedin: "https://www.linkedin.com/feed/")
+mia = User.find_or_create_by(name: "Mia", age: 24, resume: "Resume on google docs", linkedin: "https://www.linkedin.com/feed/")
 date6 = DateTime.new(2019, 8, 20)
 date7 = DateTime.new(2019, 9, 21)
 date8 = DateTime.new(2019, 10, 22)
 date9 = DateTime.new(2019, 11, 23)
 date10 = DateTime.new(2019, 12, 24)
-Job.create(title: "Amazon Software Developer", description: "Full Stack Developer", status: "Waiting for phone interview", rating: 5, application_date: date6, source: "indeed.com", user_id: mia.id)
-Job.create(title: "Google Software Developer", description: "Full Stack Developer", status: "In person interview", rating: 5, application_date: date7, source: "indeed.com", user_id: mia.id)
-Job.create(title: "Microsoft Software Developer", description: "Frontend Developer", status: "Waiting to hear back", rating: 5, application_date: date8, source: "monster.com", user_id: mia.id)
-Job.create(title: "Qualtrics Software Developer", description: "Backend Developer", status: "Technical Interview", rating: 5, application_date: date9, source: "Glenn", user_id: mia.id)
-Job.create(title: "Netflix Software Developer", description: "Full Stack Developer", status: "Waiting for phone interview", rating: 5, application_date: date10, source: "indeed.com", user_id: mia.id)
+Job.find_or_create_by(title: "Amazon Software Developer", description: "Full Stack Developer", status: "Waiting for phone interview", rating: 5, application_date: date6, source: "indeed.com", user_id: mia.id)
+Job.find_or_create_by(title: "Google Software Developer", description: "Full Stack Developer", status: "In person interview", rating: 5, application_date: date7, source: "indeed.com", user_id: mia.id)
+Job.find_or_create_by(title: "Microsoft Software Developer", description: "Frontend Developer", status: "Waiting to hear back", rating: 5, application_date: date8, source: "monster.com", user_id: mia.id)
+Job.find_or_create_by(title: "Qualtrics Software Developer", description: "Backend Developer", status: "Technical Interview", rating: 5, application_date: date9, source: "Glenn", user_id: mia.id)
+Job.find_or_create_by(title: "Netflix Software Developer", description: "Full Stack Developer", status: "Waiting for phone interview", rating: 5, application_date: date10, source: "indeed.com", user_id: mia.id)
 
 
-nicole = User.create(name: "nicole", age: 25, resume: "Resume on google docs", linkedin: "https://www.linkedin.com/feed/")
+nicole = User.find_or_create_by(name: "nicole", age: 25, resume: "Resume on google docs", linkedin: "https://www.linkedin.com/feed/")
 date11 = DateTime.new(2020, 4, 20)
 date12 = DateTime.new(2020, 4, 21)
 date13 = DateTime.new(2020, 5, 22)
 date14 = DateTime.new(2020, 5, 23)
 date15 = DateTime.new(2020, 6, 24)
-Job.create(title: "Amazon Software Developer", description: "Full Stack Developer", status: "Waiting for phone interview", rating: 5, application_date: date11, source: "indeed.com", user_id: nicole.id)
-Job.create(title: "Google Software Developer", description: "Full Stack Developer", status: "In person interview", rating: 5, application_date: date12, source: "indeed.com", user_id: nicole.id)
-Job.create(title: "Microsoft Software Developer", description: "Frontend Developer", status: "Waiting to hear back", rating: 5, application_date: date13, source: "monster.com", user_id: nicole.id)
-Job.create(title: "Qualtrics Software Developer", description: "Backend Developer", status: "Technical Interview", rating: 5, application_date: date14, source: "Glenn", user_id: nicole.id)
-Job.create(title: "Netflix Software Developer", description: "Full Stack Developer", status: "Waiting for phone interview", rating: 5, application_date: date15, source: "indeed.com", user_id: nicole.id)
+Job.find_or_create_by(title: "Amazon Software Developer", description: "Full Stack Developer", status: "Waiting for phone interview", rating: 5, application_date: date11, source: "indeed.com", user_id: nicole.id)
+Job.find_or_create_by(title: "Google Software Developer", description: "Full Stack Developer", status: "In person interview", rating: 5, application_date: date12, source: "indeed.com", user_id: nicole.id)
+Job.find_or_create_by(title: "Microsoft Software Developer", description: "Frontend Developer", status: "Waiting to hear back", rating: 5, application_date: date13, source: "monster.com", user_id: nicole.id)
+Job.find_or_create_by(title: "Qualtrics Software Developer", description: "Backend Developer", status: "Technical Interview", rating: 5, application_date: date14, source: "Glenn", user_id: nicole.id)
+Job.find_or_create_by(title: "Netflix Software Developer", description: "Full Stack Developer", status: "Waiting for phone interview", rating: 5, application_date: date15, source: "indeed.com", user_id: nicole.id)
+
 
 
 
